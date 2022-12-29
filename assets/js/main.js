523ba946b24f94ff5a2a39938afc9da30e01e4d2
Rasel Rana <107293742+raselrana12054678@users.noreply.github.com>
2022-12-29 22:58:19 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    $(".project-popup").magnificPopup({
+    type: 'image',
+  gallery:{
+    enabled: true
+  }
+  });
+  });
+
+  $(document).ready(function() {
+    $(".project-popup1").magnificPopup({
+    type: 'image',
+  gallery:{
+    enabled: true
+  }
+  });
+  });
+
+  $(document).ready(function() {
+    $(".project-popup2").magnificPopup({
+    type: 'image',
+  gallery:{
+    enabled: true
+  }
+  });
+  });
+
+  $(document).ready(function() {
+    $(".project-popup3").magnificPopup({
+    type: 'image',
+  gallery:{
+    enabled: true
+  }
+  });
+  });
+
+  $(document).ready(function() {
+    $(".project-popup4").magnificPopup({
+    type: 'image',
+  gallery:{
+    enabled: true
+  }
+  });
+  });
+
+  $(document).ready(function() {
+    $(".project-popup5").magnificPopup({
+    type: 'image',
+  gallery:{
+    enabled: true
+  }
+});
+$('.testimonials').slick({
+  prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-left-long"></i></button>',
+  nextArrow: '<button type="button" class="slick-next"><i class="fa fa-right-long"></i></button>',
+  dots: true,
+});
+  });
+
+
+  jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
+
+
+$(document).ready(function() {
+  $(".video").magnificPopup({
+  type: 'iframe',
+gallery:{
+  enabled: true
+}
+});
+});
